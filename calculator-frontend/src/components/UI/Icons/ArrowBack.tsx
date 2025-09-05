79d4cedd9de258fd13777267653ee96df482a85b
AmEl <amirelkanov0504@gmail.com>
2025-09-06 02:17:27 +0300

    media queries

--- calculator-frontend/src/components/UI/Icons/ArrowBack.tsx
+++ calculator-frontend/src/components/UI/Icons/ArrowBack.tsx
@@ -1,6 +1,12 @@
 export const ArrowBack = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="none">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="64"
+      height="64"
+      viewBox="0 0 64 64"
+      fill="none"
+    >
       <g clipPath="url(#a)">
         <path
           fill="#fff"
