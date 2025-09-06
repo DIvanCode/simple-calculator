7a53f3ae90342c0d0135281ac953d7fab691553e
AmEl <amirelkanov0504@gmail.com>
2025-09-06 14:22:03 +0300

    cosmetic fixes

--- calculator-frontend/src/API/CalculateService.ts
+++ calculator-frontend/src/API/CalculateService.ts
@@ -3,7 +3,9 @@
 export default class CalculateService {
   static async calculate(expr: string): Promise<{ data: { result: string } }> {
     //return await axios.post("/api/calculate", { expr });
+
     // TODO: mocked for now
+    console.warn('[MOCK] Mocking calculation...');
     return await new Promise<{ data: { result: string } }>((resolve) =>
       setTimeout(() => {
         resolve({
@@ -11,12 +13,25 @@ export default class CalculateService {
             result: String(eval(expr)),
           },
         });
-        // reject(new Error("Something went wrong"));
+        // reject(new Error("Something went wrong")); // uncomment & add reject to func to mock error
       }, 2000)
     );
   }
 
   static async getHistory() {
-    console.log(`TODO: get history`);
+    // return await axios.get("/api/history");
+    // TODO: mocked for now
+    console.warn('[MOCK] Mocking history...');
+    return await new Promise((resolve) =>
+      setTimeout(() => resolve({
+        data: {
+          history: [
+            { expr: '1+1', res: '2' },
+            { expr: '1+1', res: '2' },
+            { expr: '1+1', res: '2' },
+          ]
+        }
+      }), 2000)
+    );
   }
 }
